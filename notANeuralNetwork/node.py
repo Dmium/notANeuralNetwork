c5e16324f6740a8e724e0847d647025125a99062
dmium <dominiczy.hall@gmail.com>
2018-03-04 04:45:11 +0000

    Begun creating node class but too lazy to write the train method

---
+++ notANeuralNetwork/node.py
@@ -0,0 +1,23 @@
+import typing
+class Node:
+    def __init__(self):
+        self.output_states = {}
+        self.subnodes = {}
+
+    def traverse_node(self, input_state, output_state):
+        if self.subnodes[input_state] is not None:
+            self.subnodes[input_state].update(output_state)
+            return self.subnodes[input_state]
+        else:
+            self.subnodes[input_state] = Node()
+            self.subnodes[input_state].update(output_state)
+            return self.subnodes[input_state]
+
+    def update(self, output_state):
+        if self.output_states[output_state] is not None:
+            self.output_states[output_state] += 1
+        else:
+            self.output_states[output_state] = 1
+
+    def train_node(self, eval_method, input_states, goaloutput_states, weight):
+        eval_method
